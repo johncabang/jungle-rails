959672cbc86abd9203ec7e09d8157675c431ac31
johncabang <johncabz@gmail.com>
2020-11-13 05:46:54 +0000

    Add about us page/route

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
     post   :remove_item
   end
 
+  resource :about, only: [:show] do
+    root to: 'about#show'
+  end
+
   resources :orders, only: [:create, :show]
 
   namespace :admin do
